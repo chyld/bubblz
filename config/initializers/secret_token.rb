9d39cf6209fd8fdcc6010b18db1703af3b932532
Chyld Medford <chyld.medford@gmail.com>
2012-12-05 14:30:34 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bubblz::Application.config.secret_token = '3195eebf28bb8fb81a3e1c19b312e5f1860b9543e43be31be4d98329d0b8d7573b8c12ab7fd7a0d2cbe8f1f5503731110c9a0358620f4eaae2f7762edfc94f7a'
